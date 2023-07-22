31fcd0a2bc5931fe420d9fdd47834b8c5327fd54
xpresstoparadise <axwell777@gmail.com>
2023-07-23 01:24:10 +0300

    Рабочая версия версия создания письма

--- DocTypes/S1000D_4.1.0/PythonScripts/outlook.py
+++ DocTypes/S1000D_4.1.0/PythonScripts/outlook.py
@@ -2,30 +2,33 @@
 # -*- coding: UTF-8 -*-
 
 
-import win32com.client as win
-from bs4 import BeautifulSoup
+import os
+import win32com.client as win32
 
 
-def send_message(theme, To, Body, Att_path, Attachment=False):
-    path_signature = os.getenv('APPDATA') + '\\Microsoft\\Signatures\\Подпись.htm'
-    signature = html.parse(path_signature).xpath('//body')[0].text_content()
-    app = win.Dispatch("Outlook.Application")
-    # Создания письма
-    mess = app.CreateItem(0)
-    mess.To = "sakhfdsmваввbvavxcdasdbvxcbut" \
-              "" \
-              "" \
-              "khjkhjkdsdffsdfsdainov@gerdasdsafgdgdmesgroup.com"
-    # В копию
+class OutlookEmail:
+    def __init__(self, recipient, subject, body):
+        self.recipient = recipient
+        self.subject = subject
+        self.body = body
+
+    def send_email(self):
+        outlook = win32.Dispatch('Outlook.Application')
+        mail = outlook.CreateItem(0)
+        mail.To = self.recipient
+        mail.Subject = self.subject
+        mail.Body = self.body
+
+        # Add signature to the email
+        signature_path = os.path.join(os.getenv('APPDATA') + '\\Microsoft\Signatures\Подпись.htm')
+        print(signature_path)
+        if os.path.isfile(signature_path):
+            with open(signature_path, 'r') as f:
+                signature_html = f.read()
+            mail.HTMLBody = self.body + signature_html
+
+        mail.Display(True)
 
 
-    fdsfdff
-
-
-    mess.CC = "kamalov@germesgroup.com"
-    # Тема
-    mess.Subject = theme + ' - ОТК'
-    # Тело
-    mess.Body = 'Добрый день\n\nДокументы размещены в папке {}\n\n {}'.format(self.server_directory, signature)
-    # Отправка
-    mess.Send()
+email = OutlookEmail('recipient@example.com', 'Test email', 'Hello, world!')
+email.send_email()
